09f7d398495d88c81a421b590bd2caa30c56ecec
Michael Seyedian <seyedian.mike@gmail.com>
2021-12-13 10:16:30 -0700

    Add variables for cat model

---
+++ modeling.rb
@@ -0,0 +1,20 @@
+## Modeling a cat
+# strings
+name = "Penny"
+temperment = "the baddest"
+color = "tuxedo"
+
+# integers & floats
+height_inches = 8
+length_inches = 20.5
+weight_lbs = 10.25
+
+# booleans
+domestic = true
+outdoor = false
+spoiled = true
+
+# arrays
+diet = ["wet food", "dry food", "grasshoppers"]
+interests = ["bird watching", "strings", "hide & seek"]
+enemies = ["Minnow"]
